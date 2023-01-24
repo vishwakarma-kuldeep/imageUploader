f22a51453216187ca4b557c9f2fa1ddbfdd6c9e5
tescharonicIT <info@techaronic.com>
2023-01-24 18:49:44 +0530

    first commit

--- aws.js
+++ aws.js
@@ -1,96 +1,128 @@
-const AWS = require("aws-sdk");
-const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
-const uuid = require("uuid").v4;
+// const AWS = require("aws-sdk");
+// const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+// const uuid = require("uuid").v4;
+// const fs = require("fs");
+// const path = require("path");
+// require("dotenv").config();
+// // Replace this with your AWS configuration
+// const {
+//   AWS_ACCESS_KEY_ID,
+//   AWS_SECRET_ACCESS_KEY,
+//   AWS_REGION,
+//   AWS_BUCKET_NAME,
+// } = process.env;
+// const S3 = new AWS.S3({
+//   accessKeyId: AWS_ACCESS_KEY_ID,
+//   secretAccessKey: AWS_SECRET_ACCESS_KEY,
+//   region: AWS_REGION,
+// });
+
+// exports.s3Uploadv2 = async (files) => {
+//   const s3 = new S3();
+
+//   const params = files.map((file) => {
+//     return {
+//       Bucket: process.env.AWS_BUCKET_NAME,
+//       Key: `uploads/${uuid()}-${file.originalname}`,
+//       Body: file.buffer,
+//     };
+//   });
+
+//   // ${uuid()}-
+//   const data = await Promise.all(
+//     params.map((param) => s3.upload(param).promise())
+//   );
+//   console.log(data);
+//   return data;
+// };
+
+// exports.s3Uploadv3 = async (files) => {
+//   const s3client = new S3Client();
+
+//   const params = files.map((file) => {
+//     return {
+//       Bucket: process.env.AWS_BUCKET_NAME,
+//       Key: `uploads/${file.originalname}`,
+//       Body: file.buffer,
+//     };
+//   });
+
+//   return await Promise.all(
+//     params.map((param) => s3client.send(new PutObjectCommand(param)))
+//   );
+// };
+
+// // Uploading single file
+// exports.s3UploadSingle = async (file) => {
+//   try {
+//     console.log(first);
+//     const s3client = new S3Client();
+//     const params = {
+//       Bucket: process.env.AWS_BUCKET_NAME,
+//       Key: `uploads/${file.originalname}`,
+//       Body: file.buffer,
+//     };
+//     return await s3client.send(new PutObjectCommand(params));
+//   } catch (err) {
+//     console.log("Error in uploading file to s3", err.message);
+//   }
+// };
+// exports.uploadFile = async (file, originalUrl, userId) => {
+//     try {
+//        console.log(
+//          "==================================== Files  AWS File ================================"
+//         );
+//         console.log(file);
+//          console.log(
+//            "==================================== Complete AWS ================================"
+//          );
+//     const params = {
+//       Bucket: AWS_BUCKET_NAME,
+//       Key: `${originalUrl}/${userId}/` + file.originalname.replace(/ /g, "-"),
+//       Body: file.buffer,
+//       ACL: "public-read",
+//         };
+//         console.log(params)
+//         const fileUrl = await S3.upload(params).promise();
+//          console.log(
+//            "==================================== Files URL at AWS ================================"
+//         );
+//         console.log(fileUrl.Location);
+        
+//     return fileUrl.Location;
+//   } catch (error) {
+//     console.log("Error in uploading file to s3", error.message);
+//   }
+// };
+
+
+const aws = require("aws-sdk");
 const fs = require("fs");
-const path = require("path");
 require("dotenv").config();
-// Replace this with your AWS configuration
-const {
-  AWS_ACCESS_KEY_ID,
-  AWS_SECRET_ACCESS_KEY,
-  AWS_REGION,
-  AWS_BUCKET_NAME,
-} = process.env;
-const S3 = new AWS.S3({
-  accessKeyId: AWS_ACCESS_KEY_ID,
-  secretAccessKey: AWS_SECRET_ACCESS_KEY,
-  region: AWS_REGION,
+aws.config.update({
+  accessKeyId: process.env.ACCESS_KEY,
+  secretAccessKey: process.env.ACCESS_SECRET,
+  region: process.env.REGION,
 });
 
-exports.s3Uploadv2 = async (files) => {
-  const s3 = new S3();
+let uploadFile = async (file, baseUrl) => {
+  return new Promise(function (resolve, reject) {
+    let s3 = new aws.S3();
 
-  const params = files.map((file) => {
-    return {
-      Bucket: process.env.AWS_BUCKET_NAME,
-      Key: `uploads/${uuid()}-${file.originalname}`,
-      Body: file.buffer,
-    };
-  });
-
-  // ${uuid()}-
-  const data = await Promise.all(
-    params.map((param) => s3.upload(param).promise())
-  );
-  console.log(data);
-  return data;
-};
-
-exports.s3Uploadv3 = async (files) => {
-  const s3client = new S3Client();
-
-  const params = files.map((file) => {
-    return {
-      Bucket: process.env.AWS_BUCKET_NAME,
-      Key: `uploads/${file.originalname}`,
-      Body: file.buffer,
-    };
-  });
-
-  return await Promise.all(
-    params.map((param) => s3client.send(new PutObjectCommand(param)))
-  );
-};
-
-// Uploading single file
-exports.s3UploadSingle = async (file) => {
-  try {
-    console.log(first);
-    const s3client = new S3Client();
-    const params = {
-      Bucket: process.env.AWS_BUCKET_NAME,
-      Key: `uploads/${file.originalname}`,
-      Body: file.buffer,
-    };
-    return await s3client.send(new PutObjectCommand(params));
-  } catch (err) {
-    console.log("Error in uploading file to s3", err.message);
-  }
-};
-exports.uploadFile = async (file, originalUrl, userId) => {
-    try {
-       console.log(
-         "==================================== Files  AWS File ================================"
-        );
-        console.log(file);
-         console.log(
-           "==================================== Complete AWS ================================"
-         );
-    const params = {
-      Bucket: AWS_BUCKET_NAME,
-      Key: `${originalUrl}/${userId}/` + file.originalname.replace(/ /g, "-"),
-      Body: file.buffer,
+    var uploadParams = {
       ACL: "public-read",
-        };
-        console.log(params)
-        const fileUrl = await S3.upload(params).promise();
-         console.log(
-           "==================================== Files URL at AWS ================================"
-        );
-        console.log(fileUrl.Location);
-        
-    return fileUrl.Location;
-  } catch (error) {
-    console.log("Error in uploading file to s3", error.message);
-  }
+      Bucket: process.env.AWS_BUCKET_NAME,
+      Key: `${baseUrl}/${file.originalname}`,
+      Body: file.buffer,
+    };
+    s3.upload(uploadParams, function (err, data) {
+      if (err) {
+        return reject({ error: err });
+      }
+
+      return resolve(data.Location);
+    });
+  });
 };
+
+module.exports = { uploadFile };
--- index.js
+++ index.js
@@ -1,61 +1,190 @@
-require("dotenv").config();
+// require("dotenv").config();
+// const express = require("express");
+// const app = express();
+
+// const bodyParser = require("body-parser");
+// const cors = require("cors");
+// const multer = require("multer");
+// // Normalize Port for Deployment
+// const PORT = process.env.PORT || 3000;
+// const uploadFileRoute = require("./apiRoute");
+// const NormalizePort = (port) => {
+//   const parsedPort = parseInt(port, 10);
+//   if (isNaN(parsedPort)) {
+//     return port;
+//   }
+//   if (parsedPort >= 0) {
+//     return parsedPort;
+//   }
+//   return false;
+// };
+
+// // Setup server port
+// const port = NormalizePort(PORT);
+
+// // Use Node.js body parsing middleware
+// app.use(bodyParser.json({
+//     limit: "500mb",
+// }));
+// app.use(
+//   bodyParser.urlencoded({
+//       extended: true,
+//       limit: "500mb",
+//   })
+// );
+
+// // Enable CORS
+// app.use(cors({
+//     origin: "*",
+//     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+// }));
+// app.use(multer().any());
+
+// // Set development server
+// if (process.env.NODE_ENV === "development") {
+//   const morgan = require("morgan");
+//   app.use(morgan("dev"));
+// }
+
+// // Set production server
+// if (process.env.NODE_ENV === "production") {
+//   const helmet = require("helmet");
+//   app.use(helmet());
+// }
+
+// // Set base route
+// app.use("/api", uploadFileRoute);
+
+// // Start server
+// app.listen(port, () => {
+//   console.log(`Server running on port ${port}`);
+// });
+
 const express = require("express");
 const app = express();
-
 const bodyParser = require("body-parser");
+
 const cors = require("cors");
 const multer = require("multer");
-// Normalize Port for Deployment
-const PORT = process.env.PORT || 3000;
-const uploadFileRoute = require("./apiRoute");
-const NormalizePort = (port) => {
-  const parsedPort = parseInt(port, 10);
-  if (isNaN(parsedPort)) {
-    return port;
-  }
-  if (parsedPort >= 0) {
-    return parsedPort;
-  }
-  return false;
-};
-
-// Setup server port
-const port = NormalizePort(PORT);
-
-// Use Node.js body parsing middleware
-app.use(bodyParser.json({
-    limit: "500mb",
-}));
+const upload = require("./aws");
+const aws = require("aws-sdk");
+const cron = require("node-cron");
+const { default: axios } = require("axios");
 app.use(
-  bodyParser.urlencoded({
-      extended: true,
-      limit: "500mb",
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
+var s3 = new aws.S3({ apiVersion: "2006-03-01" });
 
-// Enable CORS
-app.use(cors({
-    origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-}));
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+    limit: "50mb",
+  })
+);
 app.use(multer().any());
 
-// Set development server
-if (process.env.NODE_ENV === "development") {
-  const morgan = require("morgan");
-  app.use(morgan("dev"));
-}
+app.post("/start-upload", async (req, res) => {
+  try {
+    console.log(req.files[0]);
+    console.log("Ho Gaya Upload ")
+    let params = {
+      Bucket: process.env.AWS_BUCKET_NAME,
+      Key: req.files[0].originalname,
+      ContentType: req.files[0].mimetype,
+    };
+    cron.schedule("* */1 * * * *", async () => {
+       console.log("Running a task every minute");
+      const uploadId = await new Promise((resolve, reject) =>
+      s3.createMultipartUpload(params, (err, uploadData) => {
+        if (err) {
+          reject(err);
+        } else {
+          return resolve(uploadData.UploadId);
+        }
+      })
+      );
+      console.log("UploadId: ", uploadId);
+      console.log("Running a task every second");
+      const CHUNK_SIZE = 10 * 1024 * 1024; // 10MB
+      const CHUNKS_COUNT = Math.floor(req.files[0].size / CHUNK_SIZE) + 1;
+      let promisesArray = [];
+      let start, end, blob;
 
-// Set production server
-if (process.env.NODE_ENV === "production") {
-  const helmet = require("helmet");
-  app.use(helmet());
-}
+      for (let index = 1; index < CHUNKS_COUNT + 1; index++) {
 
-// Set base route
-app.use("/api", uploadFileRoute);
+        start = (index - 1) * CHUNK_SIZE;
+        end = index * CHUNK_SIZE;
+        blob =
+          index < CHUNKS_COUNT
+            ? req.files[0].buffer.slice(start, end)
+            : req.files[0].buffer.slice(start);
 
-// Start server
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+        let params1 = {
+          Bucket: process.env.AWS_BUCKET_NAME,
+          Key: req.files[0].originalname,
+          PartNumber: index,
+          UploadId: uploadId,
+        };
+
+        const presignedUrl = await new Promise((resolve, reject) =>
+          s3.getSignedUrl("uploadPart", params1, (err, presignedUrl) => {
+            if (err) {
+              reject(err);
+            } else {
+              return resolve(presignedUrl);
+            }
+          })
+        );
+
+        // Send part aws server
+        let uploadResp = axios.put(presignedUrl, blob, {
+          headers: {
+            "Content-Type": req.files[0].mimetype,
+          },
+        });
+        promisesArray.push(uploadResp);
+      }
+
+      let resolvedArray = await Promise.all(promisesArray);
+
+      let uploadPartsArray = [];
+      resolvedArray.forEach((resolvedPromise, index) => {
+        uploadPartsArray.push({
+          ETag: resolvedPromise.headers.etag,
+          PartNumber: index + 1,
+        });
+      });
+      let params2 = {
+        Bucket: process.env.AWS_BUCKET_NAME,
+        Key: req.files[0].originalname,
+        MultipartUpload: {
+          Parts: uploadPartsArray,
+        },
+        UploadId: uploadId,
+      };
+      console.log(params2);
+      const data = await new Promise((resolve, reject) =>
+        s3.completeMultipartUpload(params2, (err, data) => {
+          if (err) {
+            reject(err);
+          } else {
+            return resolve(data);
+          }
+        })
+      );
+      console.log(data.Location);
+      res.send(data.Location);
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Error saving file.");
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
 });
