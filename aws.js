d86de2b0c74226e13b4b805285bb786001e2dcfe
tescharonicIT <info@techaronic.com>
2023-01-25 11:10:54 +0530

    Deploying new Build

--- aws.js
+++ aws.js
@@ -100,9 +100,9 @@ const aws = require("aws-sdk");
 const fs = require("fs");
 require("dotenv").config();
 aws.config.update({
-  accessKeyId: process.env.ACCESS_KEY,
-  secretAccessKey: process.env.ACCESS_SECRET,
-  region: process.env.REGION,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
 });
 
 let uploadFile = async (file, baseUrl) => {
